7cd6d4fe0e3bfec17364644bbac63a1d38857cb0
Homakanth <2311CS010709@mallareddyuniversity.ac.in>
2025-01-02 12:20:42 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Demo4 
+{
+    // Prints numbers from 1 to n
+    static void printNos(int n)
+    {
+        if(n > 0)
+        {
+            printNos(n - 1);
+            System.out.print(n + " ");
+        }
+        return;
+    }
+
+    // Driver Code
+    public static void main(String[] args) 
+    {
+        printNos(100);
+    }
+}
